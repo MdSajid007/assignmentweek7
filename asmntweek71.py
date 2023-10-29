cd6c35ddfd37ab8f60a9dc2deac2ddcf9fea0ac6
MdSajid007 <144212148+MdSajid007@users.noreply.github.com>
2023-10-29 13:12:29 +0900

    asmntweek71.py

--- asmntweek71.py
+++ asmntweek71.py
@@ -8,13 +8,13 @@ def middle(lst):
         return lst[1:-1]
 
 the_list = [1, 2, 3, 4]
-print("My list before call chop function =>", the_list)
+print("My list before calling the chop function =>", the_list)
 chop(the_list)
-print("My list after call chop function =>",the_list)
+print("My list after calling the chop function =>",the_list)
 
 the_list = [1, 2, 3, 4]
-print("\n My list before call middle function =>", the_list)
+print("\n My list before calling the middle function =>", the_list)
 
 result = middle(the_list)
-print("My list after call middle function =>", the_list)
-print("New list after call middle function =>", result)
+print("My list after calling the middle function =>", the_list)
+print("New list after calling the middle function =>", result)
